26fe91a1a333b73e12e50c3fdf439c531ad4ac75
aparna-tati-simformsolutions <120454411+aparna-tati-simformsolutions@users.noreply.github.com>
2024-04-16 10:22:41 +0530

    Removed extra layer code

--- app/src/main/java/com/android/sharedelementtransition/albums/AlbumDetailScreen.kt
+++ app/src/main/java/com/android/sharedelementtransition/albums/AlbumDetailScreen.kt
@@ -2,26 +2,42 @@ package com.android.sharedelementtransition.albums
 
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.android.sharedelementtransition.R
 import com.android.sharedelementtransition.lerp
 import com.android.sharedelementtransition.models.AlbumInfoModel
 import com.android.sharedelementtransition.models.HeaderParams
 import com.android.sharedelementtransition.models.SharedElementData
 import com.android.sharedelementtransition.models.SharedElementParams
+import com.android.sharedelementtransition.statusBarsPaddingWithOffset
 import com.android.sharedelementtransition.toPx
+import com.android.sharedelementtransition.ui.TopMenu
 import kotlinx.coroutines.launch
 
 const val ANIM_DURATION = 500
@@ -130,15 +146,88 @@ fun AlbumDetailScreen(
             modifier = modifier,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Header(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                params = headerParams,
-                contentAlphaProvider = contentAlphaState,
-                backgroundColorProvider = surfaceColor,
-                isAppearing = isAppearing,
-                onBackClick = onBackClick,
+            SharedElementContainer(
+                modifier = modifier
+                    .fillMaxSize()
+                    .background(surfaceColor.value),
+                params = headerParams.sharedElementParams,
+                isForward = isAppearing,
+                title = {
+                    HeaderTitle(
+                        alphaProvider = contentAlphaState,
+                        onBackClick = onBackClick
+                    )
+                },
+                labels = {
+                    Labels(
+                        title = headerParams.title,
+                        author = headerParams.author,
+                        alphaProvider = contentAlphaState
+                    )
+                },
+                sharedElement = {
+                    Image(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .aspectRatio(1f),
+                        painter = painterResource(id = headerParams.coverId),
+                        contentDescription = "",
+                    )
+                }
             )
         }
+    }
+}
+
+const val TOP_MENU_TITLE1 = "Now Playing"
+
+@Composable
+fun BoxScope.HeaderTitle(
+    alphaProvider: State<Float>,
+    onBackClick: () -> Unit,
+) {
+    TopMenu(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .fillMaxWidth()
+            .graphicsLayer { alpha = alphaProvider.value }
+            .align(Alignment.TopCenter)
+            .statusBarsPaddingWithOffset(),
+        title = TOP_MENU_TITLE1,
+        iconsTint = MaterialTheme.colors.onSurface,
+        endIconResId = R.drawable.baseline_share_24,
+        onStartIconClick = onBackClick
+    )
+}
+
+@Composable
+fun BoxScope.Labels(
+    title: String,
+    author: String,
+    alphaProvider: State<Float>,
+) {
+    Column(
+        modifier = Modifier
+            .align(Alignment.Center)
+            .padding(bottom = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = title,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.graphicsLayer { alpha = alphaProvider.value },
+            style = MaterialTheme.typography.body1,
+            color = MaterialTheme.colors.onSurface,
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = author,
+            modifier = Modifier.graphicsLayer { alpha = alphaProvider.value },
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.body2,
+            color = MaterialTheme.colors.onSurface,
+        )
     }
 }
